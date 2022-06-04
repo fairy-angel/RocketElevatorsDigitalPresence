85248894eefc8d5cb97430e8d00997fdbf215034
fairy-angel <fairy.angelic113@gmail.com>
2022-06-04 00:10:51 -0400

    debug

--- jsquote.js
+++ jsquote.js
@@ -38,11 +38,11 @@ for (let i=0; i < inputCommIds.length; i++){
 }
 
 for (let i=0; i < inputCorpIds.length; i++){
-    document.getElementById(inputCorpIds[i]).addEventListener("change", calculate_corporate_elevators);
+    document.getElementById(inputCorpIds[i]).addEventListener("change", calculate_corporhybrid_elevators);
 }
 
 for (let i=0; i < inputHybridIds.length; i++){
-    document.getElementById(inputHybridIds[i]).addEventListener("change",calculate_hybrid_elevators);
+    document.getElementById(inputHybridIds[i]).addEventListener("change",calculate_corporhybrid_elevators);
 }
 
 
@@ -53,7 +53,9 @@ document.getElementById("standard").addEventListener("click",recalculate)
 document.getElementById("premium").addEventListener("click",recalculate) 
 document.getElementById("excelium").addEventListener("click",recalculate) 
 
+//EVENT LISTENERS
 
+//CALCULATORS
 
 function dropDown(){
     let select = document.getElementById("building-type-options");
@@ -86,48 +88,67 @@ function dropDown(){
     
 }
 
-
-//CALCULATOR 
 function calculate_residential_elevators(){
     let apartmentCount = document.getElementById("res-number-of-apartments").value;
     let floorCount = document.getElementById("res-number-of-floors").value;
     let columnCount = Math.ceil(floorCount/20);
     let average_apt_per_floor = apartmentCount / floorCount;
     let elevatorTotal = Math.floor(average_apt_per_floor / 6); 
-    
+    console.log()
+
         elevatorTotal == elevatorTotal * columnCount;
+
+
         document.getElementById('elevator-amount-input').setAttribute('value',elevatorTotal);
         recalculate()
-
-        console.log(floorCount)
-    
     }
 
  function calculate_commercial_elevators(){
-   // let apartmentCount = document.getElementById("comm-number-of-apartments").value;
-    //let floorCount = document.getElementById("comm-number-of-floors").value;
-    
+
+       let elevatorTotal = document.getElementById('comm-number-of-elevators').value;
+
         document.getElementById('elevator-amount-input').setAttribute('value',elevatorTotal);
         recalculate()
  }
 
- function calculate_corporate_elevators(){
-   // let apartmentCount = document.getElementById("").value;
- //   let floorCount = document.getElementById("corp-number-of-floors").value;
-    
+ function calculate_corporhybrid_elevators(){
+        let buildingType = select.options[select.selectedIndex].value;
+        let floorCount  
+        let basementCount 
+        let occupantsPerFloor 
+        
+        if (buildingType== "corporateOption"){
+            floorCount = document.getElementById('corp-number-of-floors').value;
+            basementCount = document.getElementById('corp-number-of-basements').value;
+            occupantsPerFloor = document.getElementById('corp-maximum-occupancy').value;
+
+        } 
+        else if(buildingType == "hybridOption"){
+            floorCount = document.getElementById('hyb-number-of-floors').value;
+            basementCount = document.getElementById('hyb-number-of-basements').value;
+            occupantsPerFloor = document.getElementById('hyb-maximum-occupancy').value;
+
+        }
+
+        let totalFloors = basementCount + floorCount
+        let totalOccupants = totalFloors * occupantsPerFloor
+        let elevatorColumns = totalFloors / 20
+        let elevatorCount = totalOccupants / 1000
+        let elevatorsPerColumn = elevatorCount / elevatorColumns
+        let elevatorTotal = elevatorColumns * elevatorsPerColumn
+
         document.getElementById('elevator-amount-input').setAttribute('value',elevatorTotal);
         recalculate()
+        //number of occupants + basements = total occupants
+        //number of elevators = number of occupants / 1000
+        //number of floors + basements/20 = number elevator columns
+        //number of elevators / number of columns = number of elevators per column
+        //total number of elevators=number of elevators per column * number of columns
+        
 
- }
-
-function calculate_hybrid_elevators(){
- //   let apartmentCount = document.getElementById("hyb-number-of-apartments").value;
-   // let floorCount = document.getElementById("hyb-number-of-floors").value;
    
-        document.getElementById('elevator-amount-input').setAttribute('value',elevatorTotal);
-        recalculate()
-
  }
+//CALCULATORS 
 
  //document.getElementById('elevator-amount-input').value;
 
@@ -139,13 +160,13 @@ function unitPriceCalculator(){
     let premiumPrice = 12345
     let exceliumPrice = 15400
     if(document.getElementById("standard").checked){
-       document.getElementById("elevator-unit-price").setAttribute(value,standardPrice); 
+    document.getElementById("elevator-unit-price").setAttribute('value',standardPrice); 
     }
     else if(document.getElementById("premium").checked){
-        document.getElementById("elevator-unit-price").setAttribute(value,premiumPrice); 
+        document.getElementById("elevator-unit-price").setAttribute('value',premiumPrice); 
      }
     else if(document.getElementById("excelium").checked){
-        document.getElementById("elevator-unit-price").setAttribute(value,exceliumPrice); 
+        document.getElementById("elevator-unit-price").setAttribute('value',exceliumPrice); 
      }
 
 }
@@ -154,58 +175,58 @@ function unitPriceCalculator(){
 
 // ELEVATOR AMOUNT
 
-    function elevatorAmountCalculator(){
-        let elevatorAmount = document.getElementById('elevator-amount-input').value;
+    function elevatorCostCalculator(){
+        let elevatorCount = document.getElementById('elevator-amount-input').value;
         let elevatorUnitPrice = document.getElementById("elevator-unit-price").value;
-        let totalElevatorAmount = elevatorAmount * elevatorUnitPrice; 
-
-        document.getElementById("total-elevator-price").setAttribute(value,totalElevatorAmount); 
+        let totalElevatorCost = elevatorCount * elevatorUnitPrice; 
+        document.getElementById("total-elevator-cost").setAttribute('value',totalElevatorCost); 
     }
 // ELEVATOR AMOUNT
 
 
 //INSTALLATION FEES
 function installFeeCalculator(){
-    let standardFee = .10
-    let premiumFee = .13
-    let exceliumFee = .16
-    let totalElevatorAmount = document.getElementById("total-elevator-price").value;
+    let standardFee = 0.10
+    let premiumFee = 0.13
+    let exceliumFee = 0.16
+    let totalElevatorCost = document.getElementById("total-elevator-cost").value;
     let totalFee 
     
+    
     if(document.getElementById("standard").checked){
-       totalFee= totalElevatorAmount * standardFee
-
+       totalFee = totalElevatorCost * standardFee
     }
 
     else if(document.getElementById("premium").checked){
-        totalFee= totalElevatorAmount * premiumFee
+        totalFee = totalElevatorCost * premiumFee
 
      }
 
     else if(document.getElementById("excelium").checked){
-        totalFee= totalElevatorAmount * exceliumFee
+        totalFee = totalElevatorCost * exceliumFee
 
      }
-      document.getElementById("installation-fees").setAttribute(value,totalElevatorAmount);
+      document.getElementById("installation-fees").setAttribute('value',totalFee);
 }
 //INSTALLATION FEES
 
 
 //TOTAL COST
 function totalCostCalculator(){
-    let totalElevatorPrice = document.getElementById("total-elevator-price").value;
+    let totalElevatorPrice = document.getElementById("total-elevator-cost").value;
     let totalInstallFees = document.getElementById("installation-fees").value;
-    let finalPrice = totalElevatorPrice + totalInstallFees
+    let finalPrice = parseFloat(totalElevatorPrice) + parseFloat(totalInstallFees);
   
-    document.getElementById("final-price").setAttribute(value,finalPrice);
+    document.getElementById("final-prices").setAttribute('value',finalPrice);
 
 }
-//TOTAL COST
+
 
 function recalculate(){
     unitPriceCalculator()
-    elevatorAmountCalculator()
+    elevatorCostCalculator()
     installFeeCalculator()
     totalCostCalculator()
 
 }
+//TOTAL COST
